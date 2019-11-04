9ad3554230f8860b09e4a33f927ff101d263ba85
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2019-11-04 18:12:14 -0300

    Versão final

---
+++ Reconhecimento/Analise.py
@@ -0,0 +1,129 @@
+import time
+import numpy as np
+from Functions import *
+
+#Matrizes para analise de dados
+Coleta_Arr = np.array([-1])    #-1 sinaliza o começo da leitura na matriz DataXTime_Arr
+DataXTime_Arr = [] #array aninhada pelos sets de array Coleta_Arr
+
+#intervalo de tempo para coleta de dados
+TempoColetaDeAmostras = 0.7
+DeltaT = 0
+
+def ColetaDeDados(Pico_freq):
+    global DeltaT, Coleta_Arr
+    if time.time() - DeltaT > TempoColetaDeAmostras:
+        #aninha à matriz DataXTime_Arr a nova leitura neste intervalo de tempo
+        DataXTime_Arr.append(Coleta_Arr)
+        Coleta_Arr = np.array([Pico_freq])
+        DeltaT = time.time()
+    else:
+        Coleta_Arr = np.append(Coleta_Arr, Pico_freq)
+
+def getData_Arr():
+    return DataXTime_Arr
+
+def Compare():
+    global DataXTime_Arr
+    DataXTime_ArrList = []
+    tam = len(DataXTime_Arr)
+    ArrList = searchInDB()
+    Match = []
+    for i in ArrList:
+        if i[0]>tam-5 and i[0]<tam+5:
+            Match.append(i)
+    if len(Match)>0:
+        for i in Match:
+            DataXTime_ArrList.append(getFromDB(i[1])) #i[1] -> Nome do arquivo, recebe uma lista de arrays que combinam com o padrão pesquisado
+    else:
+        print("Sem resultados que combinam!")
+        return
+    
+    index = 0
+    cont = 0
+    MediaToCompare = []
+    AcuraciaToCompare=[]
+    media = 0
+
+    for Row in DataXTime_Arr:
+        for Col in Row:
+            if Col!=0:
+                cont += 1
+                media += Col
+        if cont==0:
+            MediaToCompare.append(0)
+        else:
+            MediaToCompare.append(media/cont)
+        cont = 0
+        media = 0
+    
+    for Arr in DataXTime_ArrList:
+        MediaAtual = []
+        for Row in Arr:
+            for Col in Row:
+                if Col!=0:
+                    cont += 1
+                    media += Col
+            if cont==0:
+                MediaAtual.append(0)
+            else:
+                MediaAtual.append(media/cont)
+            cont = 0
+            media = 0
+        Acuracia = []
+        if (len(MediaToCompare))<(len(MediaAtual)):
+            C=len(MediaToCompare)
+            i=0
+            while C>i:
+                print(MediaToCompare[i],"<|>",MediaAtual[i])
+                if MediaToCompare[i] == 0 and MediaAtual[i]==0:
+                    Acuracia.append(1)
+                elif MediaToCompare[i] == 0 or MediaAtual[i]==0:
+                    Acuracia.append(0)
+                else:
+                    if MediaAtual[i]>MediaToCompare[i]:
+                        Acuracia.append((MediaToCompare[i]/MediaAtual[i])) #acuracia da comparação -> maior valor -> 1 = Media de frequencias em um determinado tempo totalmente igual
+                    else:
+                        Acuracia.append((MediaAtual[i]/MediaToCompare[i])) #acuracia da comparação -> maior valor -> 1 = Media de frequencias em um determinado tempo totalmente igual
+                i += 1
+        else:
+            A=len(MediaAtual)
+            i=0
+            while A>i:
+                print(MediaToCompare[i],"<|>",MediaAtual[i])
+                if MediaToCompare[i] == 0 and MediaAtual[i]==0:
+                    Acuracia.append(1)
+                elif MediaToCompare[i] == 0 or MediaAtual[i]==0:
+                    Acuracia.append(0)
+                else:
+                    if MediaAtual[i]>MediaToCompare[i]:
+                        Acuracia.append((MediaToCompare[i]/MediaAtual[i])) #acuracia da comparação -> maior valor -> 1 = Media de frequencias em um determinado tempo totalmente igual
+                    else:
+                        Acuracia.append((MediaAtual[i]/MediaToCompare[i])) #acuracia da comparação -> maior valor -> 1 = Media de frequencias em um determinado tempo totalmente igual
+                i += 1
+            
+        AcuraciaToCompare.append(Acuracia)
+
+    BestResult = [0,0] #melhor resultado com [índice,valor]  
+    for Ac in AcuraciaToCompare:
+        aux = 0
+        cont = 0
+        for Value in Ac:
+            print("-------------", Value)
+            if Value == 0:
+                if Ac[cont-1]>0.7:
+                    Value = 0.5
+            cont += 1
+            aux += Value
+            print(Value)
+        print(Ac,len(Ac))
+        aux = aux/len(Ac) #Media das acuracias obtidas nos intervalos de tempo
+        if BestResult[1]<aux:
+            BestResult = [index,aux]
+        print(Match[index])
+        index += 1
+    print(AcuraciaToCompare[BestResult[0]],len(AcuraciaToCompare[BestResult[0]]))
+    print("Melhor resultado encontrado: ",Match[BestResult[0]][1],"Com " + str(BestResult[1]*100) + "% de confiabilidade")
+    os.popen("explorer \"https://duckduckgo.com/?q=! "+Match[BestResult[0]][2]+"\"")
+
+    
---
+++ Reconhecimento/Functions.py
@@ -0,0 +1,68 @@
+import wave
+import numpy as np
+import pickle
+import os
+
+def Modo():
+    print("Selecione O modo:")
+    print("Inserir no banco de dados Por arquivo   -> 0")
+    print("Inserir no banco de dados Por microfone -> 1")
+    print("Comparar por arquivo --------------------> 2")
+    print("Comparar por microfone ------------------> 3")
+    escolha = input()
+    if escolha == "0" or escolha == "1" or escolha == "2" or escolha == "3":
+        return int(escolha)
+    else:
+        print("Valor inválido")
+        return Modo()
+
+def getFilename():
+    try:
+        print("Insira o caminho do arquivo")
+        filename=str(input()) #audiocheck.net_sin_500Hz_-3dBFS_5s.wav
+        wf = wave.open(filename, 'rb')
+        wf.close()
+        return filename
+    except:
+        print("Diretório inválido")
+        return getFilename()
+
+def SaveInDB(Arr):
+    print("insira um nome para o padrão obtido:")
+    filename=str(input())+".freq"
+    print("insira uma descrção para o padrão:")
+    desc=str(input())
+    FreqList = []
+    #atualizando o indexador de arquivos
+    direc = os.getcwd()+"\\BD\\FreqList.list"
+    if os.path.getsize(direc) > 0:
+        file = open(direc, 'rb') 
+        FreqList = pickle.load(file)
+        file.close()
+    FreqList.append([len(Arr),filename,desc])
+    file = open(direc, "wb") 
+    pickle.dump(FreqList, file)
+    file.close()
+    #salvando
+    direc = os.getcwd() + '\\BD\\' + filename
+    file = open(direc, 'wb') 
+    pickle.dump(Arr, file)
+    file.close()
+    print("Salvo com sucesso!")
+    
+
+def getFromDB(filename):
+    direc = os.getcwd()+"\\BD\\"+filename
+    file = open(direc, 'rb') 
+    Arr = pickle.load(file)
+    file.close()
+    return Arr
+
+def searchInDB():
+    FreqList = []
+    direc = os.getcwd()+"\\BD\\FreqList.list"
+    if os.path.getsize(direc) > 0:
+        file = open(direc, 'rb') 
+        FreqList = pickle.load(file)
+        file.close()
+    return FreqList
